eaac0019d91228689026be1843c411f50e1dc260
Juho Friman <juho.friman@solita.fi>
2016-05-25 08:34:43 +0300

    Initial project setup.
    Does: nothing

---
+++ src/main/java/fi/solita/memschool/Application.java
@@ -0,0 +1,13 @@
+package fi.solita.memschool;
+
+/**
+ * Created by juhofr on 25/05/16.
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        // 1. reserve some memory
+        // 2. keep program running
+        // 3. inspect with jvisualvm
+    }
+}
