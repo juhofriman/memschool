cca2ff7e17c84fb9536005206cd66aa107dc2979
Juho Friman <juho.friman@solita.fi>
2016-05-27 14:49:36 +0300

    Filling list with UUID:s until eternity
    Just try regurlarly and with -XX:-UseGCOverheadLimit

--- src/main/java/fi/solita/memschool/Application.java
+++ src/main/java/fi/solita/memschool/Application.java
@@ -1,20 +1,26 @@
 package fi.solita.memschool;
 
+import java.math.BigInteger;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+
 /**
  * Created by juhofr on 25/05/16.
  */
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Filling StringBuffer 'till eternity...");
-        StringBuffer stringBuffer = new StringBuffer();
+        System.out.println("Filling List with UUID 'till eternity...");
+        LinkedList<UUID> foos = new LinkedList<>();
         while(true) {
+            foos.add(UUID.randomUUID());
             try {
-                Thread.sleep(10);
-                stringBuffer.append("foo");
+                Thread.sleep(1);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
+
 }
